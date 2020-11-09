0011fe47b8b5f307967b8e67aef83f226cc1806b
Miguel Arriola <miguelarriola007@gmail.com>
2020-11-08 22:20:40 -0600

    agrega async await

--- js/app.js
+++ js/app.js
@@ -55,18 +55,27 @@ function mostrarAlerta(mensaje) {
   }
 }
 
-function buscarImagenes() {
+async function buscarImagenes() {
   const termino = document.querySelector('#termino').value;
 
   const key = 'aqui va el api key';
   const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`;
 
-  fetch(url)
-    .then((respuesta) => respuesta.json())
-    .then((resultado) => {
-      totalPaginas = calcularPaginas(resultado.totalHits);
-      mostrarImagenes(resultado.hits);
-    });
+  // fetch(url)
+  //   .then((respuesta) => respuesta.json())
+  //   .then((resultado) => {
+  //     totalPaginas = calcularPaginas(resultado.totalHits);
+  //     mostrarImagenes(resultado.hits);
+  //   });
+
+  try {
+    const respuesta = await fetch(url);
+    const resultado = await respuesta.json();
+    totalPaginas = calcularPaginas(resultado.totalHits);
+    mostrarImagenes(resultado.hits);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function* crearPaginador(total) {
